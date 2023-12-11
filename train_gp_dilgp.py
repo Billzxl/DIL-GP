2aa4e05f5b75212baa9af2809a27cc59adb23e15
xilong <long22702059@qq.com>
2023-12-11 14:11:10 +0000

    update

--- train_gp_dilgp.py
+++ train_gp_dilgp.py
@@ -1,7 +1,4 @@
 import argparse
-
-import sklearn
-import sklearn.datasets as sk_data
 import numpy as np
 import random
 import torch
@@ -12,18 +9,31 @@ from torch.optim import SGD
 # from get_my_data import get_dataset
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
 from torch.distributions.normal import Normal
 from matplotlib.transforms import Bbox
 
 parser = argparse.ArgumentParser(description='OODGP Tuning')
+
+parser.add_argument('--model_name',
+                    help='choose model, gp or dilgp',
+                    default='dilgp',
+                    choices= ['gp','dilgp'],
+                    type=str)
+
+parser.add_argument('--dataset_name',
+                    help='choose dataset',
+                    default='synthetic',
+                    choices= ['synthetic'],
+                    type=str)
+
+
 parser.add_argument('--envlr',
                     help='learning rate for env_w',
                     default=0.001,
                     type=float)
 parser.add_argument('--gplr',
                     help='learning rate for gp parameters',
-                    default=0.001,
+                    default=0.005,
                     type=float)
 parser.add_argument('--epoch',
                     help='learning epoch for gp parameters',
@@ -71,9 +81,8 @@ def setup_seed(seed):
     # torch.backends.cudnn.deterministic = True
 
 # setup_seed(0)
-dataset_name = 'synthetic'
-model_name = 'oodgp'
-# model_name = 'gp'
+
+
 
 def get_dataset(dataset_name, s):
     setup_seed(0)
@@ -100,14 +109,10 @@ def test(gp, test_data, test_label):
 
     var = torch.clamp(var, min=1e-10, max=1e10)
     std = torch.sqrt(var)
-    dis  = Normal(mu.reshape(-1, 1), std.reshape(-1, 1))
-
-    log_ll = dis.log_prob(test_label)
 
     mu = mu.detach().numpy().flatten()
     std = torch.sqrt(var).detach().numpy().flatten()
     
-    # error = np.absolute(mu - test_label.numpy())
     mse_error = np.square(mu - test_label.numpy().flatten())
     error = np.sqrt(mse_error.mean())
 
@@ -119,11 +124,15 @@ def test(gp, test_data, test_label):
 
     ratio = ((smaller_than_up & larger_than_down) * 1.0).sum() / len(test_label)
 
-    return error, std.mean(), ratio, log_ll.mean().detach().cpu().numpy()
+    return error,  ratio
 
 
 
-def test_plot(gp, train_data, train_label, test_data, test_label, error):
+def plot(gp, train_data, train_label, test_data, test_label, error):
+    
+    if not os.path.exists('./debug/'):
+        os.makedirs('./debug/')
+    
     grid = torch.linspace(-3, 8.5, 200)[:,None]
     grid1 = torch.linspace(-3, 3.5, 200)[:,None]
     grid2 = torch.linspace(3.5, 8.5, 200)[:,None]
@@ -143,54 +152,32 @@ def test_plot(gp, train_data, train_label, test_data, test_label, error):
     plt.rcParams.update({'font.size': 20})
     plt.subplots(figsize=(8, 3))
     plt.ylim(-4, 4)
-    # plt.axvline(x=3.5, color='grey', linestyle='--',linewidth = 2)
+    
     plt.plot(grid.flatten(), mu, color='purple', linewidth=2,alpha=0.8)
     gt1 =  (torch.sin(grid1 * 2 * np.pi /4).flatten()*3).numpy().flatten()
     gt2 = (-torch.sin((grid2-6.5) * 2 * np.pi / 64).flatten() + 0.5).numpy().flatten()
-    gt3 = (-torch.sin((grid3-6.5) * 2 * np.pi / 64).flatten() + 0.5).numpy().flatten()
-    # plt.plot(grid1.flatten(), gt1, color='black', linewidth=1,alpha=1)
-    # plt.plot(grid2.flatten(), gt2, color='black', linewidth=1,alpha=1)
+
     plt.fill_between(grid.flatten(), y1=mu+std, y2=mu-std, alpha=0.5, color='mediumslateblue')
-    # plt.scatter(train_data.flatten(), train_label, 
-    #             c=np.array((64, 14, 50)).reshape(1, 3)/255, s=25)
+
     plt.scatter(test_data.flatten(), test_label, 
              c='orange', s=15)
     plt.scatter(train_data.flatten(), train_label, 
                 c='blue', s=15)
 
-    # plt.scatter(test_data.flatten(), test_label, 
-    #          c=np.array((242, 102, 171)).reshape(1, 3)/255, s=25)
-    # plt.title(f'After hyperparameter optimization, error={error:.4f}')
     plt.xticks([])
     plt.yticks([])
-    # plt.text(2.5,3,f'DIL-GP, RMSE={error:.4f}')
-   
-    # plt.tight_layout()
-    # plt.tight_layout()
-    if model_name == 'oodgp':
+
+    if opt.model_name == 'dilgp':
         plt.text(2.5,3,f'DIL-GP, RMSE={error:.4f}')
         # plt.title(f'DIL-GP, RMSE={error:.4f}')
-        plt.savefig(f'debug/6_train_oodgp_result.png', bbox_inches=Bbox.from_bounds(0.99, 0.31, 6.23, 2.34))
-        #Bbox.from_bounds(0.5, 0, 6, 4.7))
-        # plt.title(f'IRL-GP, error={error:.4f}')
+        plt.savefig(f'debug/dilgp_result.png', bbox_inches=Bbox.from_bounds(0.99, 0.31, 6.23, 2.34))
+
     else:
         # plt.title(f'GP, RMSE={error:.4f}')
         plt.text(3.5,3,f'GP, RMSE={error:.4f}')
-        plt.savefig(f'debug/6_train_gp_result.png',
+        plt.savefig(f'debug/gp_result.png',
                     bbox_inches=Bbox.from_bounds(0.99, 0.31, 6.23, 2.34))
-        # plt.title(f'GP, error={error:.4f}')
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+              
     plt.cla()  
     
     plt.subplots(figsize=(8, 1))
@@ -203,153 +190,37 @@ def test_plot(gp, train_data, train_label, test_data, test_label, error):
     plt.fill_between(grid2.flatten(), y1=mu2-gt2, y2=0, alpha=0.5, color='mediumslateblue')
     plt.xticks([])
     plt.yticks([])
-    # plt.axvline(x=3.5, color='grey', linestyle='--',linewidth = 2)
-    # plt.tight_layout()
-    if model_name == 'oodgp':
-    
-        # plt.title(f'DIL-GP, RMSE={error:.4f}')
-        plt.savefig(f'debug/3_train_oodgp_result_cp.png', bbox_inches=Bbox.from_bounds(0.99, 0.09, 6.23, 0.8))
-        #Bbox.from_bounds(0.5, 0, 6, 4.7))
-        # plt.title(f'IRL-GP, error={error:.4f}')
-    else:
-        # plt.title(f'GP, RMSE={error:.4f}')
-     
-        plt.savefig(f'debug/3_train_gp_result_cp.png',
-                    bbox_inches=Bbox.from_bounds(0.99, 0.09, 6.23, 0.8))
-        # plt.title(f'GP, error={error:.4f}')     
-        
-        
-        
-        
-    plt.cla()  
-    
-    plt.subplots(figsize=(4, 1))
-    plt.ylim(-0.3, 0.3)
-    # print(type(mu))
-    # print(type(gt1))
-    # plt.plot(grid1.flatten(), mu1-gt1, color='purple', linewidth=2,alpha=1)
-    plt.plot(grid3.flatten(), mu3-gt3, color='purple', linewidth=2,alpha=1)
-    # plt.fill_between(grid1.flatten(), y1=mu1-gt1, y2=0, alpha=0.5, color='mediumslateblue')
-    plt.fill_between(grid3.flatten(), y1=mu3-gt3, y2=0, alpha=0.5, color='mediumslateblue')
-    plt.xticks([])
-    plt.yticks([])
-    # plt.axvline(x=3.5, color='grey', linestyle='--',linewidth = 2)
-    # plt.tight_layout()
-    if model_name == 'oodgp':
-    
-        # plt.title(f'DIL-GP, RMSE={error:.4f}')
-        plt.savefig(f'debug/3_train_oodgp_result_cp_part.png', bbox_inches=Bbox.from_bounds(0.48, 0.09, 3.15, 0.8))
-        #Bbox.from_bounds(0.5, 0, 6, 4.7))
-        # plt.title(f'IRL-GP, error={error:.4f}')
-    else:
-        # plt.title(f'GP, RMSE={error:.4f}')
-     
-        plt.savefig(f'debug/3_train_gp_result_cp_part.png',
-                    bbox_inches=Bbox.from_bounds(0.48, 0.09, 3.15, 0.8))
-        # plt.title(f'GP, error={error:.4f}')
-        
-    plt.cla()  
-    
-    
-    
-    
-    
-    
-    
-    
 
-    if model_name == 'oodgp':
-        env_train = gp.env_w.flatten()
-        env1 = (env_train.sigmoid() < 0.5)
-        env2 = (env_train.sigmoid() >= 0.5)
-        plt.plot(train_data.flatten()[env1], train_label[env1], '.', color='blue')
-        plt.plot(train_data.flatten()[env2], train_label[env2], '.', color='yellow')
 
-        plt.savefig(f'debug/train_oodgp_env.png')
-
-        plt.cla()
 
 
 def main():
     global opt
 
-    error_diff_seed = []
-    ratio_diff_seed = []
-    ll_diff_seed = []
-    std_diff_seed = []
-    error0 = None
-    ratio0 = None
-    ll0 = None
-    std0 = None
-    # for s in range(10):
-    gplrs = [0.01, 0.003, 0.001, 0.0003, 0.0001] # exp in paper
-    # gplrs = [0.0008,0.02, 0.003,0.005, 0.001,0.002, 0.0003,0.0006, 0.0001,0.0002]
-    # gplrs = [0.001]
-    for i, gplr in enumerate(gplrs):
-        opt.gplr = gplr
-        train_data, train_label, valid_data, valid_label = get_dataset('syn',0)
-        setup_seed(0)
-        # ONLY for regression !
-        # TODO： add classification
-        if model_name == 'oodgp':
-            gp = OODGP(opt.envlr, opt.eistep, opt.lambdae, opt.usekmeans,
-                    opt.length_scale, opt.noise_scale, opt.amplitude_scale)#.cuda()
-        else:
-            gp = GP(opt.length_scale, opt.noise_scale, opt.amplitude_scale)
+    train_data, train_label, valid_data, valid_label = get_dataset('abc',0)
+    setup_seed(opt.seed)
 
-        optimizer = SGD(gp.parameters(), lr=opt.gplr)
+    if opt.model_name == 'dilgp':
+        gp = OODGP(opt.envlr, opt.eistep, opt.lambdae, opt.usekmeans,
+                opt.length_scale, opt.noise_scale, opt.amplitude_scale)#.cuda()
+    else:
+        gp = GP(opt.length_scale, opt.noise_scale, opt.amplitude_scale)
 
-        gp.fit(train_data, train_label)
+    optimizer = SGD(gp.parameters(), lr=opt.gplr)
 
-        l_error = []; l_std = []
-        test_error, test_std, _, _ = test(gp, valid_data, valid_label)
-        l_error.append(test_error.mean())
-        l_std.append(test_std.mean())
+    gp.fit(train_data, train_label)
 
-        l_loss = []; l_length = []; l_noise = []; l_amp = [];
-        for i in tqdm(range(opt.epoch)):
-            d_train = gp.train_step(train_data, train_label, optimizer)
-            l_loss.append(d_train['loss'])
-            l_length.append(d_train['length'])
-            l_noise.append(d_train['noise'])
-            l_amp.append(d_train['amplitude'])
-            # print(d_train['loss'])
-            with torch.no_grad():
-                test_error, test_std, ratio, ll = test(gp, valid_data, valid_label)
-                l_error.append(test_error.mean())
-                l_std.append(test_std.mean())
+    for i in tqdm(range(opt.epoch)):
+        gp.train_step(train_data, train_label, optimizer)
 
-        # print('l_error', l_error) # TODO: check other standards?
-        error = l_error[-1]
-        test_plot(gp, train_data, train_label, valid_data, valid_label, error)
+    with torch.no_grad():
+        test_error, ratio = test(gp, valid_data, valid_label)
 
-        error_diff_seed.append(error)
-        ratio_diff_seed.append(ratio)
-        ll_diff_seed.append(ll)
-        std_diff_seed.append(test_std)
-        print(error, ratio, ll, test_std)
+    plot(gp, train_data, train_label, valid_data, valid_label, test_error)
 
-        if np.abs(gplr - 0.001) < 1e-10:
-            error0  = error
-            ratio0 = ratio
-            ll0 = ll
-            std0 = test_std
-
-    print(error_diff_seed)
-    diff = np.abs(np.array(error_diff_seed) - error0)
-    print('mean_error,max d')
-    print(error0, diff.max())
-
-    diff = np.abs(np.array(ratio_diff_seed) - ratio0)
-    print(ratio0, diff.max())
-
-    # diff = np.abs(np.array(ll_diff_seed) - ll0)
-    # print(ll0, diff.max())
-
-    # diff = np.abs(np.array(std_diff_seed) - std0)
-    # print(std0, diff.max())
-
-
+    print('Model: ',opt.model_name)
+    print('RMSE: ',test_error)
+    print('Coverage Rate: ', ratio)
 
 
 if __name__ == "__main__":
